97aede1f5f34804304739de8ffe24b75cbc2b1a8
DESKTOP-VFPIQ4E\81803 <kagerou1215@softbank.ne.jp>
2021-09-24 22:57:07 +0900

    test

--- SceneGame.h
+++ SceneGame.h
@@ -3,6 +3,20 @@
 class CSceneGame : public CSceneBase
 {
 private:
+	int playerX = 100;
+	int playerY = 100;
+
+	int distancePlayer = 0;
+
+	int stressBarX = 700;
+	int stressBarY = 10;
+	int stressBarChenge = 300;
+
+	int stressNumber = 100;
+
+	int timeCnt = 0;
+
+	//CTexture stressGauge;
 public:
 	CSceneGame();
 	~CSceneGame();
@@ -10,5 +24,7 @@ public:
 	void Update() override;
 	void Render() override;
 	void Release() override;
+
+	
 };
 
