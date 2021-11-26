010433f0c492c964ff72d8d6bbfdfa7b5d047720
OICNET\C0433 <arussura35@gmail.com>
2021-11-26 15:05:08 +0900

    魚群のアニメーション追加

--- ShoalSardine.h
+++ ShoalSardine.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "ObstacleBase.h"
-#define  SHOALSARDINE_RECT_TOP_ADJ 150
+#define  SHOALSARDINE_RECT_TOP_ADJ 0
 #define  SHOALSARDINE_RECT_LEFT_ADJ 0
-#define  SHOALSARDINE_RECT_RIGHT_ADJ -0
-#define  SHOALSARDINE_RECT_BUTTOM_ADJ -150
+#define  SHOALSARDINE_RECT_RIGHT_ADJ 446
+#define  SHOALSARDINE_RECT_BUTTOM_ADJ 286
 
 class CShoalSardine :public CObstacleBase
 {
 private:
 	bool warningLineShow;
+
+	//アニメーション
+	CSpriteMotionController motion;
 public:
 	CShoalSardine();
 	~CShoalSardine();
@@ -20,7 +23,7 @@ public:
 	void Release()override;
 	CRectangle GetRect() {
 		return CRectangle(pos.x + SHOALSARDINE_RECT_LEFT_ADJ, pos.y + SHOALSARDINE_RECT_TOP_ADJ,
-			pos.x + Texture.GetWidth() + SHOALSARDINE_RECT_RIGHT_ADJ, pos.y + Texture.GetHeight() + SHOALSARDINE_RECT_BUTTOM_ADJ);
+			pos.x  + SHOALSARDINE_RECT_RIGHT_ADJ, pos.y + SHOALSARDINE_RECT_BUTTOM_ADJ);
 	}
 	void SetShow(bool flg)
 	{
